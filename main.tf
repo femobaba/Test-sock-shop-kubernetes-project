3c24e2fb863ffa001251e18dfefeccfed53866fe
victor.eseyin <victor.eseyin@cloudhight.com>
2023-06-30 19:28:17 -0400

    add haproxy module

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ module "vpc" {
 
 module "jenkins" {
   source           = "./module/jenkins"
-  instance_type_t2 = var.instance_type_t2
+  instance_type_t2 = "t2.medium"
   keypair_name     = module.vpc.keypair
   prt_sn1          = module.vpc.prtsub1_id
   jenkins_name     = "${local.project-name}-jenkins"
@@ -85,4 +85,23 @@ module "master_node" {
   keypair_name   = module.vpc.keypair
   instance_count = 3
   instance_name  = "${local.project-name}-master_node"
+}
+
+#create haproxy module 
+module "haproxy-servers" {
+  source        = "./module/HAproxy"
+  keypair       = module.vpc.keypair
+  ami           = "ami-0ecc74eca1d66d8a6"
+  instance_type = "t2.medium"
+  prtsub1_id    = module.vpc.prtsub1_id
+  prtsub2_id    = module.vpc.prtsub3_id
+  HAproxy_sg    = module.vpc.master_sg_id
+  master1       = module.master_node.master_ip[0]
+  master2       = module.master_node.master_ip[1]
+  master3       = module.master_node.master_ip[2]
+  master4       = module.master_node.master_ip[0]
+  master5       = module.master_node.master_ip[1]
+  master6       = module.master_node.master_ip[2]
+  name-tags     = "${local.project-name}-haproxy1"
+  name-tags2    = "${local.project-name}-haproxy-backup"
 }
--- module/haproxy/main.tf
+++ module/haproxy/main.tf
@@ -1,10 +1,10 @@
 #Create HA Proxy Server
 resource "aws_instance" "HAProxy1" {
-  ami                         = var.redhat_ami
+  ami                         = var.ami
   instance_type               = var.instance_type
   subnet_id                   = var.prtsub1_id
   vpc_security_group_ids      = [var.HAproxy_sg]
-  key_name                    = var.keypair_name
+  key_name                    = var.keypair
   user_data                   = templatefile("./module/haproxy/data.sh",{
     master1=var.master1,
     master2=var.master2,
@@ -18,11 +18,11 @@ resource "aws_instance" "HAProxy1" {
 
 #Create HA Proxy Server
 resource "aws_instance" "HAProxy-backup" {
-  ami                         = var.ubuntu_ami
+  ami                         = var.ami
   instance_type               = var.instance_type
   subnet_id                   = var.prtsub2_id
   vpc_security_group_ids      = [var.HAproxy_sg]
-  key_name                    = var.keypair_name
+  key_name                    = var.keypair
   user_data                   = templatefile("./module/haproxy/data2.sh",{
     master4=var.master4,
     master5=var.master5,
