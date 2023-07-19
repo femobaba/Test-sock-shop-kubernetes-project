f71e35239b84bae2a6aa650c229f1bd2f8dd3a77
DaddyNifemi <kehindeotogunwa@gmail.com>
2023-07-19 00:22:18 -0400

    clean code

--- module/prod_lb/main.tf
+++ module/prod_lb/main.tf
@@ -12,7 +12,7 @@ resource "aws_lb" "prod-lb" {
 }
 resource "aws_lb_target_group" "prod-tg" {
   name     = "prod-tg"
-  port     = 30001
+  port     = 30002
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   health_check {
@@ -50,18 +50,18 @@ resource "aws_lb_listener" "prod-listener2" {
 resource "aws_lb_target_group_attachment" "prod-attachmt1" {
   target_group_arn = aws_lb_target_group.prod-tg.arn
   target_id        = var.instance1
-  port             = 30001
+  port             = 30002
 }
 
 resource "aws_lb_target_group_attachment" "prod-attachmt2" {
   target_group_arn = aws_lb_target_group.prod-tg.arn
   target_id        = var.instance2
-  port             = 30001
+  port             = 30002
 }
 
 resource "aws_lb_target_group_attachment" "prod-attachmt3" {
   target_group_arn = aws_lb_target_group.prod-tg.arn
   target_id        = var.instance3
-  port             = 30001
+  port             = 30002
 }
 
