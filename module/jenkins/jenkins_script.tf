fce611c0b22bb6ff1c80bdab1300bedbdcded144
fmbaba2904 <femi.oyetola@cloudhight.com>
2023-07-21 00:35:34 -0400

    update jenkins script

--- module/jenkins/jenkins_script.tf
+++ module/jenkins/jenkins_script.tf
@@ -2,16 +2,16 @@ locals {
   jenkins-userdata = <<-EOF
 #!/bin/bash
 sudo yum update -y
-sudo yum upgrade -y
-sudo yum install wget -y
 sudo yum install git -y
-sudo yum install java-11-openjdk -y
-sudo wget https://get.jenkins.io/redhat/jenkins-2.411-1.1.noarch.rpm
-sudo rpm -ivh jenkins-2.411-1.1.noarch.rpm
-sudo yum update -y
+sudo yum install wget -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install java-17-openjdk -y
 sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
 sudo systemctl start jenkins
-sudo systemctl enable jenkins 
 sudo hostnamectl set-hostname Jenkins
 EOF
 }
